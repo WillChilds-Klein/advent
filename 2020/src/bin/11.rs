68a8a34cf7b82e9c27d599c7641e6ccfe38d478e
Will Childs-Klein <childw@amazon.com>
2021-06-26 14:17:04 -0500

    Solved 11 pt. 2

--- 2020/src/bin/11.rs
+++ 2020/src/bin/11.rs
@@ -17,12 +17,21 @@ fn main() {
 
     let acc = match vers.as_str() {
         "01" => one(&seats),
+        "02" => two(&seats),
         _ => panic!("Unsupported vers: {}", vers)
     };
     println!("{}", acc);
 }
 
 fn one(seats: &Vec<Vec<char>>) -> u64 {
+    return process(seats, false);
+}
+
+fn two(seats: &Vec<Vec<char>>) -> u64 {
+    return process(seats, true);
+}
+
+fn process(seats: &Vec<Vec<char>>, vis: bool) -> u64 {
     print_seats(seats, "ORIGINAL");
     let mut prev_occupied = 0;
     let mut occupied = 1;
@@ -30,26 +39,31 @@ fn one(seats: &Vec<Vec<char>>) -> u64 {
     let mut next = seats.clone();
     while occupied != prev_occupied {
         prev_occupied = occupied;
-        occupied = iterate(&curr, &mut next);
+        occupied = iterate(&curr, &mut next, vis);
         curr = next.clone();
     }
     print_seats(&next, "FINAL");
     return occupied;
 }
 
-fn iterate(seats: &Vec<Vec<char>>, next: &mut Vec<Vec<char>>) -> u64 {
+fn iterate(seats: &Vec<Vec<char>>, next: &mut Vec<Vec<char>>, vis: bool) -> u64 {
     let mut total_occupied: u64 = 0;
+    let occupied_threshold = if vis {5} else {4};
     for ii in 0..seats.len() {
         for jj in 0..seats[0].len() {
-            let adj_occupied = adjacent_occupied(seats, ii as isize, jj as isize);
+            let occupied = if vis {
+                visible_occupied(seats, ii as isize, jj as isize)
+            } else {
+                adjacent_occupied(seats, ii as isize, jj as isize)
+            };
             match seats[ii][jj] {
                 'L' => {
-                    if adj_occupied == 0 {
+                    if occupied == 0 {
                         next[ii][jj] = '#';
                     }
                 },
                 '#' => {
-                    if adj_occupied >= 4 {
+                    if occupied >= occupied_threshold {
                         next[ii][jj] = 'L';
                     }
                 },
@@ -63,7 +77,6 @@ fn iterate(seats: &Vec<Vec<char>>, next: &mut Vec<Vec<char>>) -> u64 {
             }
         }
     }
-    // print_seats(next, "NEXT");
     return total_occupied;
 }
 
@@ -81,6 +94,46 @@ fn adjacent_occupied(seats: &Vec<Vec<char>>, row_idx: isize, col_idx: isize) -> 
         }
     }
     return occupied_count;
+}
+
+fn visible_occupied(seats: &Vec<Vec<char>>, row_idx: isize, col_idx: isize) -> u64 {
+    let mut count = 0;
+    let transforms = [
+        |x: isize| x-1, // decrement
+        |x: isize| x,   // identity
+        |x: isize| x+1, // increment
+    ];
+    for rt in transforms.iter() {
+        for ct in transforms.iter() {
+            if rt(0) == 0 && ct(0) == 0 { // skip if both rt and ct are identity, else infinite loop
+                continue;
+            } else if line_of_sight(seats, row_idx, col_idx, rt, ct) {
+                count += 1;
+            }
+        }
+    }
+    return count;
+}
+
+fn line_of_sight(
+    seats: &Vec<Vec<char>>, row: isize, col: isize, rt: &impl Fn(isize) -> isize, ct: &impl Fn(isize) -> isize
+) -> bool {
+    let mut ii = row;
+    let mut jj = col;
+    loop {
+        ii = rt(ii);
+        jj = ct(jj);
+        if !(ii >= 0 && ii < seats.len() as isize && jj >= 0 && jj < seats[0].len() as isize) {
+            return false;
+        }
+        if ii == row && jj == col { // don't consider origin coordinates
+            continue;
+        } else if seats[ii as usize][jj as usize] == '#' {  // occupied seat visible
+            return true;
+        } else if seats[ii as usize][jj as usize] == 'L' {  // empty seat visible
+            return false;
+        }
+    }
 }
 
 fn print_seats(seats: &Vec<Vec<char>>, label: &str) {
