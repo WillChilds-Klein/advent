9b5bf72c5401c3c64c24f8d5be8ae66e8443d403
Will Childs-Klein <childw@amazon.com>
2021-08-09 11:03:45 -0400

    Solved 17 pt. 2

--- 2020/src/bin/17.rs
+++ 2020/src/bin/17.rs
@@ -19,11 +19,16 @@ fn main() {
         )
         .collect();
     let initial_dimension = cmp::max(flat.len(), flat[0].len());
-    let mut board = vec![vec![vec![false; initial_dimension]; initial_dimension]; 3];
-    board[1] = flat.clone();
+
+    let mut board_3d = vec![vec![vec![false; initial_dimension]; initial_dimension]; 3];
+    board_3d[1] = flat.clone();
+
+    let mut board_4d = vec![vec![vec![vec![false; initial_dimension]; initial_dimension]; 3]; 3];
+    board_4d[1][1] = flat.clone();
 
     let acc = match vers.as_str() {
-        "01" => one(&board, 6),
+        "01" => one(&board_3d, 6),
+        "02" => two(&board_4d, 6),
         _ => panic!("Unsupported vers: {}", vers)
     };
     println!("{}", acc);
@@ -31,11 +36,11 @@ fn main() {
 
 fn one(init_board: &Vec<Vec<Vec<bool>>>, iters: i32) -> i32 {
     let mut board = init_board.clone();
-    print_board(&board);
+    print_board_3d(&board);
     for _ in 0..iters {
-        board = expand(&board);
-        board = iterate(&board);
-        print_board(&board);
+        board = expand_3d(&board);
+        board = iterate_3d(&board);
+        print_board_3d(&board);
     }
     let mut active_count = 0;
     for z in 0..board.len() {
@@ -48,7 +53,7 @@ fn one(init_board: &Vec<Vec<Vec<bool>>>, iters: i32) -> i32 {
     active_count
 }
 
-fn count_active_neighbors(board: &Vec<Vec<Vec<bool>>>, z: i32, y: i32, x: i32) -> i32 {
+fn count_active_neighbors_3d(board: &Vec<Vec<Vec<bool>>>, z: i32, y: i32, x: i32) -> i32 {
     let mut count = 0;
     for ii in cmp::max(0,z-1)..cmp::min(z+2,board.len() as i32) {
         for jj in cmp::max(0,y-1)..cmp::min(y+2,board[0].len() as i32) {
@@ -63,12 +68,12 @@ fn count_active_neighbors(board: &Vec<Vec<Vec<bool>>>, z: i32, y: i32, x: i32) -
     count
 }
 
-fn iterate(board: &Vec<Vec<Vec<bool>>>) -> Vec<Vec<Vec<bool>>> {
+fn iterate_3d(board: &Vec<Vec<Vec<bool>>>) -> Vec<Vec<Vec<bool>>> {
     let mut ret = board.clone();
     for z in 0..board.len() {
         for y in 0..board[0].len() {
             for x in 0..board[0][0].len() {
-                let active_neighbors = count_active_neighbors(board, z as i32, y as i32, x as i32);
+                let active_neighbors = count_active_neighbors_3d(board, z as i32, y as i32, x as i32);
                 ret[z][y][x] = if board[z][y][x] {
                     active_neighbors == 2 || active_neighbors == 3
                 } else {
@@ -80,7 +85,7 @@ fn iterate(board: &Vec<Vec<Vec<bool>>>) -> Vec<Vec<Vec<bool>>> {
     ret
 }
 
-fn expand(board: &Vec<Vec<Vec<bool>>>) -> Vec<Vec<Vec<bool>>> {
+fn expand_3d(board: &Vec<Vec<Vec<bool>>>) -> Vec<Vec<Vec<bool>>> {
     let mut ret = board.clone();
     let [mut left, mut right, mut top, mut bottom, mut front, mut back] = [false; 6];
     for z in 0..board.len() {
@@ -102,7 +107,7 @@ fn expand(board: &Vec<Vec<Vec<bool>>>) -> Vec<Vec<Vec<bool>>> {
                     if x == 0 && !left {
                         left = true;
                     }
-                    if x == board.len()-1 && !right {
+                    if x == board[0][0].len()-1 && !right {
                         right = true;
                     }
                 }
@@ -121,7 +126,6 @@ fn expand(board: &Vec<Vec<Vec<bool>>>) -> Vec<Vec<Vec<bool>>> {
         let (d, w) = (ret.len(), ret[0][0].len());
         for z in 0..d {
             ret[z].insert(0, vec![false; w]);
-
         }
     }
     if bottom {
@@ -149,7 +153,7 @@ fn expand(board: &Vec<Vec<Vec<bool>>>) -> Vec<Vec<Vec<bool>>> {
     ret
 }
 
-fn print_board(board: &Vec<Vec<Vec<bool>>>) {
+fn print_board_3d(board: &Vec<Vec<Vec<bool>>>) {
     for y in 0..board[0].len() {
         print!("|");
         for z in 0..board.len() {
@@ -161,4 +165,155 @@ fn print_board(board: &Vec<Vec<Vec<bool>>>) {
         println!();
     }
     println!("{:-<1$}", "", board[0][0].len()*board.len() + board.len()+1);
+}
+
+fn two(init_board: &Vec<Vec<Vec<Vec<bool>>>>, iters: i32) -> i32 {
+    let mut board = init_board.clone();
+    for _ in 0..iters {
+        board = expand_4d(&board);
+        board = iterate_4d(&board);
+    }
+    let mut active_count = 0;
+    for w in 0..board.len() {
+        for z in 0..board[0].len() {
+            for y in 0..board[0][0].len() {
+                for x in 0..board[0][0][0].len() {
+                    active_count += if board[w][z][y][x] { 1 } else { 0 };
+                }
+            }
+        }
+    }
+    active_count
+}
+
+fn count_active_neighbors_4d(board: &Vec<Vec<Vec<Vec<bool>>>>, w: i32, z: i32, y: i32, x: i32) -> i32 {
+    let mut count = 0;
+    for hh in cmp::max(0,w-1)..cmp::min(w+2,board.len() as i32) {
+        for ii in cmp::max(0, z - 1)..cmp::min(z + 2, board[0].len() as i32) {
+            for jj in cmp::max(0, y - 1)..cmp::min(y + 2, board[0][0].len() as i32) {
+                for kk in cmp::max(0, x - 1)..cmp::min(x + 2, board[0][0][0].len() as i32) {
+                    if hh == w && ii == z && jj == y && kk == x {
+                        continue;
+                    }
+                    count += if board[hh as usize][ii as usize][jj as usize][kk as usize] { 1 } else { 0 };
+                }
+            }
+        }
+    }
+    count
+}
+
+fn iterate_4d(board: &Vec<Vec<Vec<Vec<bool>>>>) -> Vec<Vec<Vec<Vec<bool>>>> {
+    let mut ret = board.clone();
+    for w in 0..board.len() {
+        for z in 0..board[0].len() {
+            for y in 0..board[0][0].len() {
+                for x in 0..board[0][0][0].len() {
+                    let active_neighbors = count_active_neighbors_4d(board, w as i32, z as i32, y as i32, x as i32);
+                    ret[w][z][y][x] = if board[w][z][y][x] {
+                        active_neighbors == 2 || active_neighbors == 3
+                    } else {
+                        active_neighbors == 3
+                    };
+                }
+            }
+        }
+    }
+    ret
+}
+
+fn expand_4d(board: &Vec<Vec<Vec<Vec<bool>>>>) -> Vec<Vec<Vec<Vec<bool>>>> {
+    let mut ret = board.clone();
+    let [mut left, mut right, mut top, mut bottom, mut front, mut back, mut before, mut after] = [false; 8];
+    for w in 0..board.len() {
+        for z in 0..board[0].len() {
+            for y in 0..board[0][0].len() {
+                for x in 0..board[0][0][0].len() {
+                    if board[w][z][y][x] {
+                        if w == 0 && !before {
+                            before = true;
+                        }
+                        if w == board.len() - 1 && !after {
+                            after = true;
+                        }
+                        if z == 0 && !front {
+                            front = true;
+                        }
+                        if z == board[0].len() - 1 && !back {
+                            back = true;
+                        }
+                        if y == 0 && !top {
+                            top = true;
+                        }
+                        if y == board[0][0].len() - 1 && !bottom {
+                            bottom = true;
+                        }
+                        if x == 0 && !left {
+                            left = true;
+                        }
+                        if x == board[0][0][0].len() - 1 && !right {
+                            right = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if before {
+        let (depth, height, width) = (ret[0].len(), ret[0][0].len(), ret[0][0][0].len());
+        ret.insert(0, vec![vec![vec![false; width]; height]; depth]);
+    }
+    if after {
+        let (depth, height, width) = (ret[0].len(), ret[0][0].len(), ret[0][0][0].len());
+        ret.push(vec![vec![vec![false; width]; height]; depth]);
+    }
+    if front {
+        let (time, height, width) = (ret.len(), ret[0][0].len(), ret[0][0][0].len());
+        for w in 0..time {
+            ret[w].insert(0, vec![vec![false; width]; height]);
+        }
+    }
+    if back {
+        let (time, height, width) = (ret.len(), ret[0][0].len(), ret[0][0][0].len());
+        for w in 0..time {
+            ret[w].push(vec![vec![false; width]; height]);
+        }
+    }
+    if top {
+        let (time, depth, width) = (ret.len(), ret[0].len(), ret[0][0][0].len());
+        for w in 0..time {
+            for z in 0..depth {
+                ret[w][z].insert(0, vec![false; width]);
+            }
+        }
+    }
+    if bottom {
+        let (time, depth, width) = (ret.len(), ret[0].len(), ret[0][0][0].len());
+        for w in 0..time {
+            for z in 0..depth {
+                ret[w][z].push(vec![false; width]);
+            }
+        }
+    }
+    if left {
+        let (time, depth, height) = (ret.len(), ret[0].len(), ret[0][0].len());
+        for w in 0..time {
+            for z in 0..depth {
+                for y in 0..height {
+                    ret[w][z][y].insert(0, false);
+                }
+            }
+        }
+    }
+    if right {
+        let (time, depth, height) = (ret.len(), ret[0].len(), ret[0][0].len());
+        for w in 0..time {
+            for z in 0..depth {
+                for y in 0..height {
+                    ret[w][z][y].push(false);
+                }
+            }
+        }
+    }
+    ret
 }
