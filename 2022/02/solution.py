0a1bd404a9ff34b0f539529893ea57c6605fcdd9
Will Childs-Klein <childw@amazon.com>
2022-12-02 14:43:33 -0500

    Solved 02 pt. 2

--- 2022/02/solution.py
+++ 2022/02/solution.py
@@ -1,9 +1,9 @@
 import sys
 
 def main():
-    scores = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
+    vals = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
     lines = open(sys.argv[1]).readlines()
-    rounds = [list(map(lambda y: scores[y], x.strip().split(' '))) for x in lines]
+    rounds = [list(map(lambda y: vals[y], x.strip().split(' '))) for x in lines]
     def calc_score(other: int, me: int) -> int:
         if (me - other) in [-2, 1]:
             return 6 + me
@@ -13,7 +13,17 @@ def main():
             return 0 + me
     scores = map(lambda x: calc_score(x[0], x[1]), rounds)
     print("PART 1: " + str(sum(scores)))
-    print("PART 2: ")
+    lose, draw, win = vals['X'], vals['Y'], vals['Z']
+    def get_move(other: int, outcome: int) -> int:
+        if outcome == lose:
+            return (other + 1) % 3 + 1
+        elif outcome == draw:
+            return other
+        elif outcome == win:
+            return other % 3 + 1
+        assert False
+    scores = [calc_score(x, get_move(x, y)) for x, y in rounds]
+    print("PART 2: " + str(sum(scores)))
 
 if __name__ == '__main__':
     main()
