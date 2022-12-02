02b482a4d8a3451a41c57a54edb33e63755a4472
Will Childs-Klein <childw@amazon.com>
2022-12-01 19:42:55 -0500

    Solved 01 pt. 2

--- 2022/01/solution.py
+++ 2022/01/solution.py
@@ -4,6 +4,8 @@ def main():
     lines = open(sys.argv[1]).read()
     counts = [sum([int(i) for i in j.split('\n')]) for j in lines.split('\n\n')]
     print("PART 1: " + str(max(counts)))
+    top_three = sum(sorted(counts)[-3:])
+    print("PART 2: " + str(top_three))
 
 if __name__ == '__main__':
     main()
