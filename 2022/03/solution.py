1d3307cb1f2c42e2949fa9f2346b7387aee00c45
Will Childs-Klein <childw@amazon.com>
2022-12-21 10:56:05 -0600

    Solved 03 pt. 2

--- 2022/03/solution.py
+++ 2022/03/solution.py
@@ -10,10 +10,15 @@ def main():
         for x in set.intersection(left, right):
             priority_sum += priority(x)
     print("PART 1: " + str(priority_sum))
-    
+    badge_sum = 0
+    for ii in range(0, len(lines), 3):
+        one, two, three = lines[ii], lines[ii+1], lines[ii+2]
+        for x in set.intersection(*map(set, map(str.strip, [one, two, three]))):
+            badge_sum += priority(x)
+    print("PART 2: " + str(badge_sum))
 
 def priority(x: str) -> int:
-    assert x and len(x) == 1
+    assert x and len(x) == 1 and x.isalpha()
     if x.upper() == x:
         return ord(x) - ord('A') + 27
     else:
