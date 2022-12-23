39174041c1f417dcb28faf8a9cba203c5d3e93d8
Will Childs-Klein <childw@amazon.com>
2022-12-23 00:13:18 -0600

    Solved 06 pt. 2

--- 2022/06/solution.py
+++ 2022/06/solution.py
@@ -1,19 +1,24 @@
 import sys
 
-WINDOW_LENGTH = 4
+PACKET_WINDOW_LENGTH = 4
+MESSAGE_WINDOW_LENGTH = 14
 
 def main():
     stream = list(str(open(sys.argv[1]).read()))
-    window = []
+    print("PART 1:", marker_index(stream, PACKET_WINDOW_LENGTH))
+    print("PART 2:", marker_index(stream, MESSAGE_WINDOW_LENGTH))
+
+def marker_index(stream: list[str], length: int) -> int:
     count = 0
     for i in range(len(stream)):
         count += 1
-        if i < WINDOW_LENGTH:
+        if i < length:
             continue
-        window = set(stream[i-WINDOW_LENGTH+1:i+1])
-        if len(window) >= WINDOW_LENGTH:
+        window = set(stream[i-length+1:i+1])
+        if len(window) >= length:
             break
-    print("PART 1:", count)
+    return count
+
 
 if __name__ == '__main__':
     main()
