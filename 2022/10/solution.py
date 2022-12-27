a0d636fc7cc7f99bf52bb508270572c53653ea8a
Will Childs-Klein <childw@amazon.com>
2022-12-27 12:35:58 -0600

    Solved 10 pt. 2

--- 2022/10/solution.py
+++ 2022/10/solution.py
@@ -13,6 +13,7 @@ def main():
     cycle, signal_sum = 0, 0
     x, instruction_cycles = 1, 0
     instruction = [None]
+    crt = [["." for _ in range(40)] for _ in range(6)]
     while len(instructions) > 0 or instruction_cycles > 0:
         # check cycle number, update signal_sum if applicable
         if (cycle + 20) % 40 == 0:
@@ -30,10 +31,20 @@ def main():
                 instruction_cycles = 2
             else:
                 raise Exception(f"Unrecognized op: {instruction[0]}")
+        row = (cycle % (len(crt) * len(crt[0]))) // len(crt[0])
+        pixel = cycle % len(crt[0])
+        crt[row][pixel] = "#" if x - 1 <= pixel <= x + 1 else "."
         # one clock tick/cycle per iteration
         instruction_cycles -= 1
         cycle += 1
     print("PART 1:", signal_sum)
+    print("PART 2:")
+    draw_crt(crt)
+
+
+def draw_crt(crt: list[list[str]]):
+    for row in crt:
+        print("".join(row))
 
 
 if __name__ == "__main__":
