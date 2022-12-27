d9514774f7643b24c96e63a6fcae13fa400ef1c0
Will Childs-Klein <childw@amazon.com>
2022-12-27 11:52:51 -0600

    Solved 10 pt. 1

---
+++ 2022/10/solution.py
@@ -0,0 +1,40 @@
+import sys
+
+
+def main():
+    lines = open(sys.argv[1]).readlines()
+    instructions = []
+    for line in lines:
+        instruction = line.split(" ")
+        instruction[0] = instruction[0].strip()
+        if len(instruction) == 2:
+            instruction[1] = int(instruction[1])
+        instructions.append(instruction)
+    cycle, signal_sum = 0, 0
+    x, instruction_cycles = 1, 0
+    instruction = [None]
+    while len(instructions) > 0 or instruction_cycles > 0:
+        # check cycle number, update signal_sum if applicable
+        if (cycle + 20) % 40 == 0:
+            signal_sum += cycle * x
+        # either init case or time for the next instruction
+        if instruction_cycles == 0:
+            # do post-processing of last instruction
+            if instruction[0] == "addx":
+                x += instruction[1]
+            # get the next instruction and set instruction_cycles accordingly
+            instruction = instructions.pop(0) if instructions else [None]
+            if instruction[0] == "noop":
+                instruction_cycles = 1
+            elif instruction[0] == "addx":
+                instruction_cycles = 2
+            else:
+                raise Exception(f"Unrecognized op: {instruction[0]}")
+        # one clock tick/cycle per iteration
+        instruction_cycles -= 1
+        cycle += 1
+    print("PART 1:", signal_sum)
+
+
+if __name__ == "__main__":
+    main()
