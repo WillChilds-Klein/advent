cc261063efd12002ae07949934b78843fa7ad9a2
Will Childs-Klein <childw@amazon.com>
2022-12-29 11:21:27 -0500

    Solved 12 pt. 2

--- 2022/12/solution.py
+++ 2022/12/solution.py
@@ -26,21 +26,48 @@ def main():
     def is_valid_height(p: Point, candidate: Point) -> bool:
         return hmap[candidate[0]][candidate[1]] - hmap[p[0]][p[1]] <= 1
 
-    def explore(s: Point, e: Point, seen: set[Point]) -> int:
-        seen.add(s)
-        if s == e:
-            return 0
-        x, y = s
-        candidates = [(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)]
-        candidates = [c for c in candidates if c not in seen]
-        candidates = [c for c in candidates if is_in_bounds(c)]
-        candidates = [c for c in candidates if is_valid_height(s, c)]
-        candidates = [explore(c, e, set(seen)) for c in candidates]
-        if not candidates:
-            return INT_MAX
-        return 1 + (candidates[0] if len(candidates) == 1 else min(*candidates))
+    edges = set()
+    for i in range(len(hmap)):
+        for j in range(len(hmap[0])):
+            p = (i, j)
+            candidates = [(i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j)]
+            candidates = [c for c in candidates if is_in_bounds(c)]
+            candidates = [c for c in candidates if is_valid_height(p, c)]
+            edges.update([(p, c) for c in candidates])
 
-    print("PART 1:", explore(s, e, set()))
+    # cf. https://en.wikipedia.org/wiki/Bellman%E2%80%93Ford_algorithm
+    # cf. https://www.notion.so/willck/Week-13-Graph-Algorithms-pt-2-b38e8b93085e4526a86303dc44172d37#62f4012220c84f748db5a0268b3b2aef
+    sssp = [[INT_MAX for _ in range(len(hmap[0]))] for _ in range(len(hmap))]
+    sssp[s[0]][s[1]] = 0
+    for _ in range(len(hmap)*len(hmap[0])):
+        for u, v in edges:
+            candidate_distance = sssp[u[0]][u[1]] + 1
+            if candidate_distance < sssp[v[0]][v[1]]:
+                sssp[v[0]][v[1]] = candidate_distance
+
+    print("PART 1:", sssp[e[0]][e[1]])
+
+    edges = set()
+    vertices = set()
+    for i in range(len(hmap)):
+        for j in range(len(hmap[0])):
+            p = (i, j)
+            vertices.add(p)
+            candidates = [(i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j)]
+            candidates = [c for c in candidates if is_in_bounds(c)]
+            # NOTE: swap p and candidate params in is_valid_height call below
+            candidates = [c for c in candidates if is_valid_height(c, p)]
+            edges.update([(p, c) for c in candidates])
+
+    sssp = [[INT_MAX for _ in range(len(hmap[0]))] for _ in range(len(hmap))]
+    sssp[e[0]][e[1]] = 0
+    for _ in range(len(hmap)*len(hmap[0])):
+        for u, v in edges:
+            candidate_distance = sssp[u[0]][u[1]] + 1
+            if candidate_distance < sssp[v[0]][v[1]]:
+                sssp[v[0]][v[1]] = candidate_distance
+
+    print("PART 2:", min(*[sssp[x][y] for x, y in vertices if hmap[x][y] == 0]))
 
 
 if __name__ == '__main__':
