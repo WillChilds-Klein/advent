8bc16fa0884b41a270b50a70b6799135750c510a
Will Childs-Klein <childw@amazon.com>
2023-01-02 18:22:36 -0500

    Solved 14 pt. 2

--- 2022/14/solution.py
+++ 2022/14/solution.py
@@ -1,4 +1,5 @@
 import dataclasses
+import json
 import sys
 import typing
 
@@ -35,60 +36,76 @@ def main():
             if p.y > min_y:
                 min_y = p.y
 
-    smap = [[None for _ in range(max_y + 1)] for _ in range(max_x + 1)]
+    # compensate for floor. additional x space given by 45º right triangle between
+    # origin, max fill point w/ floor, and point on floor directly below origin. length
+    # of edge between the latter 2 is equal to the distance from origin to floor.
+    max_y += 2
+    max_x += max_y - (max_x - ORIGIN.x)
+
+    smap_original = [[None for _ in range(max_y + 1)] for _ in range(max_x + 1)]
     for rock in rocks:
-        print(rock)
         prev = rock.pop(0)
         while rock:
-            print(rock)
             curr = rock.pop(0)
             x_diff, y_diff = curr.x - prev.x, curr.y - prev.y
-            print(x_diff, y_diff)
             if prev.x != curr.x:
                 step = x_diff // abs(x_diff)
                 for i in range(prev.x, curr.x + step, step):
-                    print(i)
-                    smap[i][curr.y] = "#"
+                    smap_original[i][curr.y] = "#"
             if prev.y != curr.y:
                 step = y_diff // abs(y_diff)
                 for i in range(prev.y, curr.y + step, step):
-                    smap[curr.x][i] = "#"
+                    smap_original[curr.x][i] = "#"
             prev = curr
 
-    def drop_sand(smap: list[list[typing.Optional[str]]]) -> bool:
+    def drop_sand(s: list[list[typing.Optional[str]]]) -> bool:
+        y_lim = len(s[0]) - 1
         x, y = ORIGIN.x, ORIGIN.y
-        while y < max_y:
-            if smap[x][y + 1] is None:
+        while y < y_lim and s[x][y] is None:
+            if s[x][y + 1] is None:
                 y += 1
-            elif smap[x - 1][y + 1] is None:
+            elif s[x - 1][y + 1] is None:
                 x -= 1
                 y += 1
-            elif smap[x + 1][y + 1] is None:
+            elif s[x + 1][y + 1] is None:
                 x += 1
                 y += 1
             else:
-                smap[x][y] = "o"
+                s[x][y] = "o"
                 return True
         return False
 
     count = 0
+    smap = json.loads(json.dumps(smap_original))
     while drop_sand(smap):
         count += 1
     # draw_map(smap)
     print("PART 1", count)
 
+    count = 0
+    smap = json.loads(json.dumps(smap_original))
+    # add the floor
+    floor_start, floor_end  = ORIGIN.x - max_y, ORIGIN.x + max_y
+    for i in range(floor_start, floor_end+1):
+        smap[i][-1] = "#"
+    while drop_sand(smap):
+        count += 1
+    # draw_map(smap)
+    print("PART 2", count)
 
-def draw_map(smap: list[list[typing.Optional[str]]]):
+
+def draw_map(s: list[list[typing.Optional[str]]]):
     min_x = 2**32
-    for y in range(len(smap[0])):
-        for x in range(len(smap)):
-            if smap[x][y] and x < min_x:
+    for y in range(len(s[0])):
+        for x in range(len(s)):
+            if s[x][y] and x < min_x:
                 min_x = x
-    for y in range(len(smap[0])):
-        for x in range(len(smap)):
+    print(len(s), len(s[0]))
+    for y in range(len(s[0])):
+        for x in range(len(s)):
             if x < min_x:
                 continue
-            print(smap[x][y] if smap[x][y] else ".", end="")
+            print(s[x][y] if s[x][y] else ".", end="")
         print()
 
 
