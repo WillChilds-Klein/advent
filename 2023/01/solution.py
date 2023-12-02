3d997cb79a16bd0938ae9761a7b9a29c07856d32
Will Childs-Klein <childw@amazon.com>
2023-12-01 20:05:06 -0500

    Solved 01 pt. 2

--- 2023/01/solution.py
+++ 2023/01/solution.py
@@ -1,13 +1,39 @@
 import sys
 
+numbers = {'one': 1,
+           'two': 2,
+           'three': 3,
+           'four': 4,
+           'five': 5,
+           'six': 6,
+           'seven': 7,
+           'eight': 8,
+           'nine': 9,
+}
+
+def is_digit(s: str) -> bool:
+    if len(s) == 1 and s[0].isdigit():
+        return True
+    if s in numbers.keys():
+        return True
+    return False
+
+def convert_digit(s: str) -> int:
+    """ assumes that |s| is a digit """
+    if len(s) == 1 and s[0].isdigit():
+        return int(s[0])
+    return numbers[s]
 
 def main():
     lines = [l for l in sys.stdin.readlines()]
     rsum = 0
     for line in lines:
-        first = int(list(filter(lambda c: c.isdigit(), line))[0])
-        last = int(list(filter(lambda c: c.isdigit(), line[::-1]))[0])
-        rsum += first*10 + last
+        digits = []
+        for i in range(len(line)+1):
+            for j in range(i, len(line)+1):
+                if is_digit(line[i:j+1]):
+                    digits.append(convert_digit(line[i:j+1]))
+        rsum += 10*digits[0] + digits[-1]
     print(rsum)
 
 if __name__ == '__main__':
