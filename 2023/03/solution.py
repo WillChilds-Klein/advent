b16edd08c3f6d385340445c9107a19b2f99d6ac3
Will Childs-Klein <childw@amazon.com>
2023-12-03 14:45:53 -0500

    Solved 03 pt. 2

--- 2023/03/solution.py
+++ 2023/03/solution.py
@@ -7,30 +7,37 @@ def main():
     lines = [l.strip() for l in sys.stdin.readlines()]
     schema = [[c for c in line] for line in lines]
 
-    def part_num(i: int, j: int) -> int:
-        if schema[i][j] is None or not schema[i][j].isdigit():
+    def get_gear(i: int, j: int) -> int:
+        if schema[i][j] != '*':
             return 0
-        digits = []
-        # determine number boundaries
-        for x in range(j, len(schema[0])):
-            if schema[i][x] is None or not schema[i][x].isdigit():
-                break
-            digits.append(schema[i][x])
-        assert all(map(str.isdigit, digits))
-        # after saving off digits, erase from |schema| so we don't overcount
-        for x in range(j, j+len(digits)):
-            assert schema[i][x].isdigit()
-            schema[i][x] = None
-        is_valid = False
-        for x in range(max(i-1, 0), min(i+1+1,len(schema))):
-            for y in range(max(j-1, 0), min(j+len(digits)+1,len(schema[0]))):
-                # did we find a symbol?
-                if schema[x][y] is not None and not schema[x][y].isdigit():
-                    is_valid = True
+        ratios = []
+        seen = [[False for _ in schema[0]] for _ in schema]
+        def extract_num(x: int, y: int) -> int:
+            start, end = y, y
+            # look backwards
+            for k in range(y, -1, -1):
+                seen[x][k] = True
+                if not schema[x][k] or not schema[x][k].isdigit():
                     break
-        if not is_valid:
-            return 0    # couldn't find any surrounding symbols
-        return int(''.join(digits))
+                start = k
+            # look forwards
+            for k in range(y, len(schema[0])):
+                seen[x][k] = True
+                if not schema[x][k] or not schema[x][k].isdigit():
+                    break
+                end = k
+            # if start == end, then the ratio is only 1 digit
+            return int(''.join(schema[x][start:(end+1)]))
+
+        for x in range(max(i-1, 0), min(i+1+1,len(schema))):
+            for y in range(max(j-1, 0), min(j+1+1,len(schema[0]))):
+                if seen[x][y] or not schema[x][y] or not schema[x][y].isdigit():
+                    continue
+                # we know here that schema[x][y] is an unseen digit
+                ratios.append(extract_num(x, y))
+        if len(ratios) != 2:
+            return 0
+        return functools.reduce(lambda x, y: x * y, ratios)
 
     # first pass clears non-symbols/non-numbers
     for i in range(len(schema)):
@@ -41,7 +48,7 @@ def main():
     rsum = 0
     for i in range(len(schema)):
         for j in range(len(schema[0])):
-            rsum += part_num(i, j)
+            rsum += get_gear(i, j)
     print(rsum)
 
 
